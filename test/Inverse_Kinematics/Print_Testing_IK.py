b62c042ba93e90d157f6dc3ff93cfa4a90e44f82
Varo <varo.wah@gmail.com>
2025-10-08 18:31:39 +0700

    trying to make Inverse Kinematics better

--- test/Inverse_Kinematics/Print_Testing_IK.py
+++ test/Inverse_Kinematics/Print_Testing_IK.py
@@ -1,12 +1,13 @@
-from classCreation import CKMJetArm
 import math
-Arm1 = CKMJetArm()
 
-L1 = 10.0
+
+L1 = 15.0
 L2 = 15.0
 DEG_PER_PULSE = 0.25 
 BASE_ZERO_OFFSET = 125.0
 ANGLE_ZERO_OFFSET = 125.0
+GROUND_OFFSET = 10.0
+HAND_OFFSET = 15.0
 ELBOW_UP = True 
 
 def angle_cosine(a, b, c, *, degrees=True):
@@ -30,13 +31,15 @@ def arm_to_pulse(arm_deg, deg_per_pulse=DEG_PER_PULSE, zero_offset=ANGLE_ZERO_OF
 
 x, y, z = map(float, input("Enter x y z: ").split())
 
+z = z + GROUND_OFFSET - HAND_OFFSET
+
 base_angle = math.degrees(math.atan2(y, x))
 l = math.hypot(x, y) # base distance
 d = math.hypot(l, z) # hypothesis including with height with distance
 
 # Elbow internal angle (at the joint between L1 and L2)
 # B in radians; 0 < B < π
-c_elbow = (L1*L1 + L2*L2 - d*d) / (2*L1*L2)
+c_elbow = angle_cosine(L1, d, L2, degrees=True)
 c_elbow = max(-1.0, min(1.0, c_elbow))
 B_rad = math.acos(c_elbow)
 
@@ -55,9 +58,8 @@ L1_angle = theta1  # this now points forward for reachable front targets
 # B: internal elbow angle between L1 and L2
 B = angle_cosine(L1, L2, d, degrees=True)
 e = 180 - B                       # elbow bend: 0=straight, 90=front-right-angle
-e_rel = e - 90 
 e_pulse = arm_to_pulse(e)
-L2_angle = e_rel
+L2_angle = e
 
  
 print(f"Elbow (L2) angle: {L2_angle:.3f}°")
