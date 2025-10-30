167f6ac9ad5368280774dae2e1838c7762a1117b
Varo <varo.wah@gmail.com>
2025-10-30 21:38:47 +0700

    making a class for the inverse kinematics

---
+++ test/Inverse Kinematics/Class Execution.py
@@ -0,0 +1,56 @@
+from classCreation import CKMJetArm
+Arm1 = CKMJetArm()
+import math
+
+class JetArmIK:
+    def __init__(self):
+        self.Arm = CKMJetArm()
+        self.L1 = 15.0
+        self.L2 = 15.0
+        self.DEG_PER_PULSE = 0.24
+        self.BASE_ZERO_OFFSET = 125.0
+        self.ANGLE_ZERO_OFFSET = 125.0
+        self.ELBOW_UP = True
+
+    def base_to_pulse(self, angle_deg):
+        return int(round(angle_deg / self.DEG_PER_PULSE + self.BASE_ZERO_OFFSET))
+
+    def arm_to_pulse(self, arm_deg):
+        return int(round(arm_deg / self.DEG_PER_PULSE + self.ANGLE_ZERO_OFFSET))
+
+    def calculate_angles(self, x, y, z):
+        base_angle = math.degrees(math.atan2(y, x))
+        l = math.hypot(x, y)
+        d = math.hypot(l, z)
+        h = d / 2
+        theta = math.degrees(math.acos(h / self.L1))
+        phi = math.degrees(math.atan2(z, l))
+
+        if self.ELBOW_UP:
+            L1_angle = phi + theta
+        else:
+            L1_angle = phi - theta
+
+        intersection = 180 - (2 * theta)
+        if self.ELBOW_UP:
+            L2_angle = intersection - 90
+        else:
+            L2_angle = 360 - (intersection + 90)
+
+        L3_angle = 90 - (L2_angle + L1_angle)
+
+        return base_angle, L1_angle, L2_angle, L3_angle
+
+    def move_to(self, x, y, z):
+        base_angle, L1_angle, L2_angle, L3_angle = self.calculate_angles(x, y, z)
+
+        base_pulse = self.base_to_pulse(base_angle)
+        L1_pulse = self.arm_to_pulse(L1_angle)
+        L2_pulse = self.arm_to_pulse(L2_angle)
+        L3_pulse = self.arm_to_pulse(L3_angle) + 60
+
+        print(f"Moving to: {base_pulse}, {L1_pulse}, {L2_pulse}, {L3_pulse}")
+        self.Arm.moveJetArm(1, base_pulse)
+        self.Arm.moveJetArm(2, L1_pulse)
+        self.Arm.moveJetArm(3, L2_pulse)
+        self.Arm.moveJetArm(4, L3_pulse)
