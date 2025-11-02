7f94cbed53b2c147d308d49b5fc74236de8f51eb
varo-wah <varo.wah@gmail.com>
2025-11-02 19:29:44 +0800

    able to access camera

--- test/Vision/camera.py
+++ test/Vision/camera.py
@@ -1,9 +1,16 @@
 import cv2
-cap = cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture(0)  # 0 = /dev/video0
+if not cap.isOpened():
+    raise Exception("Camera not found")
+
 while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    if not ret:
+        break
+    cv2.imshow("Camera Feed", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
